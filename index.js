941413451a57491ccd9508a4943df45541d7288f
viniciobb <viniciobb@gmail.com>
2018-01-09 22:57:02 -0200

    tratamento do /

--- index.js
+++ index.js
@@ -10,6 +10,35 @@ const
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 
+// Adds support for GET requests to our webhook
+app.get('/', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  //let VERIFY_TOKEN = "muitaGranaComBot"
+    
+  // Parse the query params
+  //let mode = req.query['hub.mode'];
+  //let token = req.query['hub.verify_token'];
+  //let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  //if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    //if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      //console.log('WEBHOOK_VERIFIED');
+      res.status(200).send("on line");
+    
+    //} else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      //res.sendStatus(403);      
+    //}
+  //}
+});
+
+
 app.post('/webhook', (req, res) => {  
  
   let body = req.body;
